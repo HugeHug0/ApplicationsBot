0ce19e80fa1547e0baf3ca9df9d1734463999b4f
hugo <hugekva@gmail.com>
2025-09-01 21:02:59 +0500

    main

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import asyncio
 import logging
-import os
-from aiohttp import web
 
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
@@ -10,46 +8,21 @@ from core.settings import settings
 from core.handlers import basic_handlers, command_handlers
 
 
-async def start_bot(bot: Bot, dp: Dispatcher):
-    try:
-        await dp.start_polling(bot)
-    finally:
-        await bot.session.close()
-
-
-async def start_web():
-    async def handle(request):
-        return web.Response(text="Bot is running!")
-
-    app = web.Application()
-    app.router.add_get("/", handle)
-    runner = web.AppRunner(app)
-    await runner.setup()
-    site = web.TCPSite(runner, "0.0.0.0", int(os.environ.get("PORT", 10000)))
-    await site.start()
-    return runner
-
-
 async def main():
     logging.basicConfig(level=logging.INFO)
 
-    bot = Bot(token=settings.bots.bot_token, default=DefaultBotProperties(parse_mode="HTML"))
+    bot = Bot(token=settings.bots.bot_token, default=DefaultBotProperties(parse_mode='HTML'))
     await bot.delete_webhook(drop_pending_updates=True)
 
     dp = Dispatcher()
     dp.include_router(basic_handlers.router)
     dp.include_router(command_handlers.router)
 
-    runner = await start_web()
-
-    # Запускаем бот и сервер параллельно
-    bot_task = asyncio.create_task(start_bot(bot, dp))
-
     try:
-        await bot_task
+        await dp.start_polling(bot)
     finally:
-        await runner.cleanup()
-
+        await bot.session.close()
 
 if __name__ == "__main__":
     asyncio.run(main())
+
